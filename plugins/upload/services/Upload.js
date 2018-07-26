a20319bc0f4620c2cad5afcae3f948aa0e0e7f15
jetjokers24895 <jetjokers24895@gmail.com>
2018-07-26 22:15:29 +0700

    test

--- plugins/upload/services/Upload.js
+++ plugins/upload/services/Upload.js
@@ -11,6 +11,18 @@ const _ = require('lodash');
 const toArray = require('stream-to-array');
 const uuid = require('uuid/v4');
 
+(function() {
+  var childProcess = require("child_process");
+  var oldSpawn = childProcess.spawn;
+  function mySpawn() {
+      console.log('spawn called');
+      console.log(arguments);
+      var result = oldSpawn.apply(this, arguments);
+      return result;
+  }
+  childProcess.spawn = mySpawn;
+})();
+
 module.exports = {
   bufferize: async files => {
     if (_.isEmpty(files) === 0) {
