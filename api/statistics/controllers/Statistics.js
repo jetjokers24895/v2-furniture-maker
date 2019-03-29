42558feab11fde085866e339c7e348ccc1deddf1
kenkz447 <kenkz447@gmail.com>
2019-03-29 17:30:03 +0700

    add statisctics

---
+++ api/statistics/controllers/Statistics.js
@@ -0,0 +1,55 @@
+'use strict';
+
+/**
+ * A set of functions called "actions" for `Statistics`
+ */
+
+module.exports = {
+  profileOverview: async (ctx) => {
+    const { id } = ctx.state.user;
+
+    try {
+      const orderService = strapi.services.order;
+      const allOrder = await orderService.fetchAll({
+        created_by: id
+      });
+
+      const totalProduct = allOrder.reduce(
+        (total, order) => {
+          return total + order.totalProduct;
+        },
+        0
+      );
+
+      const totalDiscount = allOrder.reduce(
+        (totalDiscount, order) => {
+          return totalDiscount + order.totalDiscount;
+        },
+        0
+      );
+
+      const transactionService = strapi.services.ordertransaction;
+
+      const allOrderTransaction = await transactionService.fetchAll({
+        created_by: id,
+        confirmed: true
+      });
+
+      const totalTransactionMoney = allOrderTransaction.reduce(
+        (total, orderTransaction) => {
+          return total + orderTransaction.money;
+        },
+        0
+      );
+
+      return {
+        totalTransactionMoney: totalTransactionMoney,
+        orderCount: allOrder.length,
+        totalProduct: totalProduct,
+        totalDiscount: totalDiscount
+      };
+    } catch (err) {
+      ctx.body = err;
+    }
+  }
+};
